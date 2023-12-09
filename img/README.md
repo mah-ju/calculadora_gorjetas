5557121bd3a8df7863c85aa04ff01a7bae9e3834
Maria J <mahjuaraujo@gmail.com>
2023-12-09 12:19:50 -0300

    second commit

---
+++ img/README.md
@@ -0,0 +1,12 @@
+# Tip Calculator 💸
+<p>This simple web application calculates the tip based on the bill amount and service quality. Users can input the bill amount, select the service quality, and click the button to get the tip amount and total bill.
+</p>
+
+## Features
+- **Calculate Tip**: Input the bill amount and service quality, and the application will calculate the tip and total bill.
+- **Clear Fields**: The fields are cleared when the page is loaded, allowing users to start fresh.
+- **Keyboard Shortcut**: Users can press the "Enter" key to calculate the tip, providing a convenient alternative to clicking the button.
+
+## Overview 
+
+![Alt Text](/img/overview.gif)
--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function calculateTip() {
     const billAmount = parseFloat(document.querySelector("#billAmount").value);
     const serviceQuality = parseFloat(document.querySelector("#serviceQuality").value);
 
+    if(!serviceQuality){
+        alert("Por favor preencha todos os dados");
+        return; 
+    }
     if(isNaN(billAmount)) {
 
         alert("Por favor preencha os dados");
@@ -20,4 +24,26 @@ function calculateTip() {
     totalAmountInput.value = `R$ ${totalAmount}`;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+   
+    const billAmountInput = document.querySelector("#billAmount");
+    const serviceQualityInput = document.querySelector("#serviceQuality");
+    const tipAmountInput = document.querySelector("#tipAmount");
+    const totalAmountInput = document.querySelector("#totalAmount");
+
+    billAmountInput.value = "";
+    serviceQualityInput.value = "";
+    tipAmountInput.value = "";
+    totalAmountInput.value = "";
+
+
+});
+
 calculateBtn.addEventListener("click", calculateTip);
+
+window.addEventListener("keydown", (e)=>{
+    if(e.key === "Enter"){
+        calculateTip();
+    }
+    });
+
